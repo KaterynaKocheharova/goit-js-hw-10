c3a269ea54e28e005646d19c4e9c97b029884461
KaterynaKocheharova <kot41901@gmail.com>
2024-03-02 17:28:27 +0200

    added library

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,22 +1,29 @@
+// INCLUDING LIBRARY
+
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+// ADDING FLATPICKER
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
+  
+const myTimePicker = flatpickr("#datetime-picker", options);
+
+
+
 // ALGORYTHM
 
 // get the currentData
 // get the data in the future
-// find the difference between two dates
-// turn on the loop, it works untill the dates are the same
+// find the difference bettween current and date in the future
+// get updated secs, minutes, hours ets at every iteration - use the loop
+// put it in html
 
-function timer(currentDate, dateInTheFuture) {
-const difference = dateInTheFuture - currentDate;
-let dateToChange = currentDate;
-console.log(dateToChange);
-
-for(let i = 0; i < difference; i++) {
-    console.log(dateToChange += 1);
-}
-
-}
-
-
-
-
-timer(new Date(), new Date("2024-05-16"));
