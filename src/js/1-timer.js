3ba28a510e296343edbfbc289095b57ce8635121
KaterynaKocheharova <kot41901@gmail.com>
2024-03-02 20:21:29 +0200

    added a little bit of timer logic

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,35 +1,74 @@
 // INCLUDING LIBRARY
 
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
-// ADDING FLATPICKER
+// VARIABLES
 
+const btn = document.querySelector('[data-start]');
+btn.disabled = true;
+
+// USER SELECTED DATE
+let userSelectedDate;
+
+// GLATPICKER WITH OPTIONS
 const options = {
-    enableTime: true,
-    // !!! don't see the difference
-    // dateFormat: "Y-m-d H:i",
-    // !!! recommended but not needed
-    // altInput: true,
-    // altFormat: "F j, Y",
-    // dateFormat: "Y-m-d",
-    // time_24hr: true,
-    defaultDate: new Date(),
-    // minuteIncrement: 1,
-    // onClose(selectedDates) {
-    //   console.log(selectedDates[0]);
-    // },
-  };
-  
-const myTimePicker = flatpickr("#datetime-picker", options);
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      alert('Please choose a date in the future');
+    } else {
+      btn.disabled = false;
+      userSelectedDate = selectedDates[0];
+      btn.addEventListener("click", backwardsTimer);
+    }
+  },
+};
 
 
+const myTimePicker = flatpickr('#datetime-picker', options);
+
+
+
+// HANDLE BACKWARDS TIMER BTN
+
+function backwardsTimer() {
+
+
+setInterval(() => {
+  let milisecTimeDif = userSelectedDate - new Date();
+  let seconds = milisecTimeDif / 1000;
+  let minutes = milisecTimeDif / 60000;
+  let hours = milisecTimeDif/ 3600000;
+  let days = milisecTimeDif /86400000;
+console.log(`Left seconds ${seconds}`)
+}, 1000)
+
+// add to spans
+}
+
+// !!! ACCESSING PROPS
+
+// console.log(typeof myTimePicker.currentYear);
+
+// Properties#
+// Once you’ve got the instance in say fp, accessing props is as simple as e.g. fp.currentYear
+
+// selectedDates#
+// The array of selected dates (Date objects).
+
+// currentYear#
+// The year currently displayed on the calendar.
+
+// currentMonth#
+// The zero-indexed month number (0-11) currently displayed on the calendar.
+
+// config#
+// The configuration object (defaults + user-specified option
 
 // ALGORYTHM
 
-// get the currentData
-// get the data in the future
-// find the difference bettween current and date in the future
-// get updated secs, minutes, hours ets at every iteration - use the loop
-// put it in html
 
