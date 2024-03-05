10f2dad6001167b1e3a9191bc94c12ae53520d73
KaterynaKocheharova <kot41901@gmail.com>
2024-03-05 20:22:54 +0200

    added some calendar css

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -26,6 +26,7 @@ let userSelectedDate;
 // ======================================= FLATPICKER OPTIONS ===============================================
 const options = {
   enableTime: true,
+  dateFormat: "Y-m-d H:i",
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
